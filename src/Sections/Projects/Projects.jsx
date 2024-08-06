2b0fa3c759ba4780263614bbcae9799df4fbf00d
RadioCat01 <pwnkanishka@gmail.com>
2024-08-06 12:21:48 +0530

    Update Projects.jsx

--- src/Sections/Projects/Projects.jsx
+++ src/Sections/Projects/Projects.jsx
@@ -10,7 +10,7 @@ import { motion } from "framer-motion"
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-        <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity: 0, y:-100}} transition={{duration:0.5}} className={styles.sectionTitle}>Projects</motion.h1>
+        <motion.h1 whileInView={{opacity:1, x:0}} initial={{opacity: 0, x:-100}} transition={{duration:0.5}} className={styles.sectionTitle}>Projects</motion.h1>
         <div className={styles.projectContainer}>
             <ProjectCard src={viberr} link={''} h3={'Viber'} p={'Streaming App'}/>
             <ProjectCard src={hipster} link={''} h3={'Hipster'} p={'Something App'}/>
