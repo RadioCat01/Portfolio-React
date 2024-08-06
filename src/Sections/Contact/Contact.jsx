2853b48877d185ecd93616723beb1b64d6bf66c1
RadioCat01 <pwnkanishka@gmail.com>
2024-08-07 00:51:45 +0530

    changes

--- src/Sections/Contact/Contact.jsx
+++ src/Sections/Contact/Contact.jsx
@@ -4,17 +4,17 @@ function Contact() {
   return (
     <section id='contact' className={style.container}>
         <h1 className='sectionTitle'>Contact</h1>
-        <form action=''>
+        <form action="https://formspree.io/f/mzzpgbzw" method="POST">
             <div className='formGroup'>
                 <label htmlFor='name' hidden>Name</label>
                 <input type='text' name='name' id='name' placeholder='Name' required/>
             </div>
             <div className='formGroup'>
-                <label htmlFor='email' hidden>Name</label>
+                <label htmlFor='email' hidden>Email</label>
                 <input type='text' name='email' id='email' placeholder='Email' required/>
             </div>
             <div className='formGroup'>
-                <label htmlFor='message' hidden>Name</label>
+                <label htmlFor='message' hidden>Message</label>
                 <textarea name='messahe' id='email' placeholder='Message' required/>
             </div>
             <input type='submit' className='hover btn' value="Submit"/>
