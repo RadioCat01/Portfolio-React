62492352754303d11ff661340036e96a7fad6c3f
RadioCat01 <pwnkanishka@gmail.com>
2024-08-06 15:11:00 +0530

    error fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import './App.css';
 import {Route, createBrowserRouter, createRoutesFromElements,RouterProvider, Navigate} from 'react-router-dom';
 import Layout from "./Pages/Layout";
 import HomePage from "./Pages/HomePage/HomePage";
-import BlogPage1 from "./Pages/BlogPage/BlogPage1/BlogPage1";
-import BlogPage2 from "./Pages/BlogPage/BlogPage2/BlogPage2";
-
 
 function App() {
 
@@ -13,8 +10,6 @@ function App() {
     createRoutesFromElements(
       <Route path="/" element={<Layout/>}>
         <Route path="/home" element={<HomePage/>}/>
-        <Route path='/SpringBoot-infinite-recursion-loop' element={<BlogPage1/>}/>
-        <Route path='/SpringBoot-dependency-injection' element={<BlogPage2/>}/>
         <Route path="/" element={<Navigate to="/home" />} />
       </Route>
     )
--- src/Sections/Blogs/Blogs.jsx
+++ src/Sections/Blogs/Blogs.jsx
@@ -9,8 +9,8 @@ function Blogs() {
     <section id="projects" className={styles.container}>
         <motion.h1 whileInView={{opacity:1, x:0}} initial={{opacity: 0, x:-100}} transition={{duration:0.5}} className={styles.sectionTitle}>Blogs</motion.h1>
         <div className={styles.projectContainer}>
-            <BlogCard src={springJpa} link={'/SpringBoot-infinite-recursion-loop'} h3={'Spring Boot'} p={'Infinite Recursion loop in data jpa'} />
-            <BlogCard src={springBoot} link={'/SpringBoot-dependency-injection'} h3={'Spring Boot'} p={'Dependency injection and SOLID principles'}/>
+            <BlogCard src={springJpa} link={'https://radiocatwrites.hashnode.dev/what-is-infinite-recursive-loop-in-spring-boot-everything-you-need-to-know'} h3={'Spring Boot'} p={'Infinite Recursion loop in data jpa'} />
+            <BlogCard src={springBoot} link={'https://radiocatwrites.hashnode.dev/spring-boot-dependency-injection-through-the-lens-of-solid-principles'} h3={'Spring Boot'} p={'Dependency injection and SOLID principles'}/>
         </div>
     </section>
   )
