d4252108aeb02e88ae9358a03992fad103eab83f
RadioCat01 <pwnkanishka@gmail.com>
2024-08-07 21:17:16 +0530

    cha

--- src/Sections/Blogs/Blogs.jsx
+++ src/Sections/Blogs/Blogs.jsx
@@ -9,7 +9,7 @@ import BlogCard from "../../common/BlogCard";
 function Blogs() {
   return (
     <section id="projects" className={styles.container}>
-        <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity: 0, y:150}} transition={{duration:0.5}} className={styles.sectionTitle}>Blogs</motion.h1>
+        <motion.h1 whileInView={{opacity:1, y:0}} initial={{opacity: 0, y:50}} transition={{duration:0.5}} className={styles.sectionTitle}>Blogs</motion.h1>
         <div className={styles.projectContainer}>
             <BlogCard src={springJpa} link={'https://radiocatwrites.hashnode.dev/what-is-infinite-recursive-loop-in-spring-boot-everything-you-need-to-know'} h3={'Spring Boot'} p={'What is infinite recursion problem in Spring data JPA?'} />
             <BlogCard src={springBoot} link={'https://radiocatwrites.hashnode.dev/spring-boot-dependency-injection-through-the-lens-of-solid-principles'} h3={'Spring'} p={'Dependency injection explained with SOLID principles'}/>
--- src/common/BlogCard.jsx
+++ src/common/BlogCard.jsx
@@ -5,7 +5,7 @@ import styles from './BlogCardStyles.module.css';
 
 function BlogCard({src, link , h3, p}) {
   return (
-    <motion.a whileInView={{opacity:1, y:0}} initial={{opacity: 0, y:100}} transition={{duration:0.5}} href={link} className={styles.container}>
+    <motion.a whileInView={{opacity:1, y:0}} initial={{opacity: 0, y:50}} transition={{duration:0.5}} href={link} className={styles.container}>
     <img className={styles.img} src={src} />
     <p className={styles.p}>{p}</p>
     <p className={styles.topic}>{h3}</p>
